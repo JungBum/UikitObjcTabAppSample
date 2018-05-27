60dcdcd5f66ef619179d08901efa62325b8fc17b
서정범 <jungbumi@naver.com>
2018-05-27 15:46:34 +0900

    xcode initialized

---
+++ UikitObjcTabAppSample/UikitObjcTabAppSample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UikitObjcTabAppSample
+//
+//  Created by 서정범 on 2018. 5. 27..
+//  Copyright © 2018년 dogrice. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
